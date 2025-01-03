0649c7aedfc8c0338d78610fcea14a7582b2eb65
nikr00ndev <ngiveros@gmail.com>
2025-01-03 23:14:59 +0700

    Add files via upload

---
+++ main/js/all.js
@@ -0,0 +1,1 @@
+var sigma = 123
---
+++ main/js/main.js
@@ -0,0 +1,11 @@
+var all = require(all.js)
+
+alert("ok")
+var st = 0;
+while (1 == 1) {
+    if (st == "1") {
+        console.log(sigma)
+    } else {
+        console.log("not admin")
+    }
+}
